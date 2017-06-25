f10205fe8d92269ffd2894c85cdc6ed79f571834
Dmitry Ryumkin <riumkin@gmail.com>
2017-06-25 20:19:53 +0300

    Add ability to add cities to the list

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import WeatherReducers from './reducer_weather';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  weather: WeatherReducers
 });
 
 export default rootReducer;
