d8dfe827f289791957af7948b9080028b89765af
Dmitry Ryumkin <riumkin@gmail.com>
2017-06-26 10:52:57 +0300

    Add another charts

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -6,15 +6,21 @@ export class WeatherList extends Component {
 
   renderWeather(cityData) {
     const name = cityData.city.name;
-    const temps = cityData.list.map(weather => weather.main.temp);
-    console.log(temps);
-
+    const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273 );
+    const pressures = cityData.list.map(pressure => pressure.main.pressure);
+    const humidities = cityData.list.map(humidity => humidity.main.pressure);
     return (
       
       <tr key={name}>
         <td>{name}</td>
         <td>
-          <Chart data={temps} color="orange" />
+          <Chart data={temps} color="orange" units="K"/>
+        </td>
+         <td>
+          <Chart data={pressures} color="green" units="hPa"/>
+        </td>
+        <td>
+          <Chart data={humidities} color="blue" units="%"/>
         </td>
       </tr>
     );
@@ -26,9 +32,9 @@ export class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temparute</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temparute (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
